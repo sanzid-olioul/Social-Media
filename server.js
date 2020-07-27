11a3cab76459c41d494f61753931ffd8e17cb64f
Sanzid Olioul <sanzid94@gmail.com>
2020-07-27 21:11:03 +0600

    MongoDB connection SetUp

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+//Connect to the Mongose.
+connectDB();
 
 app.get('/',(req,res)=> res.send("It's Working...") );
 
